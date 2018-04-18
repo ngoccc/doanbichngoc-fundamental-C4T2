ff8a65f9b07d08370d44d26d541a1f2ff4a886c3
piiiiii <kusokawaii69@gmail.com>
2018-04-18 20:56:48 +0700

    initial commit

--- Lesson4/homework/HW_3.py
+++ Lesson4/homework/HW_3.py
@@ -1,1 +1,10 @@
-# wtf
+def find_kmers(s, k):
+    kmers = []
+    for i in range(len(s) - k + 1):
+        a = s[i:i + k]
+        if a not in kmers:
+            kmers.append(s[i:i + k])
+    return print(kmers)
+
+
+find_kmers("ATCGGATTCG", 3)
--- Lesson4/homework/HW_9.py
+++ Lesson4/homework/HW_9.py
@@ -1,5 +1,34 @@
-class Lunch:
-class Customer:
 class Employee:
+    isReceivedOrder = False
+
+    def Receive_Order(self, foodName):
+        if self.isReceivedOrder == True:
+            return foodName
+
+
+class Customer:
+    food = ""
+
+    def Order(self, foodName, employee):
+        self.food = foodName
+        employee.isReceivedOrder = True
+
+
 class Food:
-# ???????
+    class Lunch:
+        lunch = ""
+
+        def __init__(self, l):
+            self.lunch = l
+
+
+ngoc = Customer()
+anhBien = Employee()
+anhQuy = Employee()
+Food.Lunch("My van than")
+Food.Lunch("Pho bo")
+Food.Lunch("Bun oc")
+Customer.Order(ngoc, "My van than", anhBien)
+Customer.Order(ngoc, "Bun oc", anhQuy)
+print(Employee.Receive_Order(anhBien, "My van than"))
+print(Employee.Receive_Order(anhQuy, "Bun oc"))
